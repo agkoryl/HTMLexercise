d01bc9a98c5a1285c3f5af6cfbc73e05fc40b269
Agnieszka Koryl <ag.koryl@gmail.com>
2018-05-26 23:54:26 +0200

    New JS

---
+++ firstWeb.js
@@ -0,0 +1,6 @@
+alert('Welcome to my first page!');
+var person = prompt("Please enter your name", "Your Name");
+
+if (person != null) {
+    alert("Hello " + person + "! Feel free to go through all my works");
+}
